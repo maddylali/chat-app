1c782ad381975d6fe02f3dccea6ca95cdee21ae1
Madhu <maddyn1983@gmail.com>
2017-09-23 18:41:12 +0530

    added socket io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
+const clientPath = path.join(__dirname, '../public')
 const app = express();
+const server = http.createServer(app);
+const port = process.env.port || 3000;
 
-console.log(__dirname + './../public');
-const clientPath = path.join(__dirname, '../public');
+const io = socketIO(server);
 
-const PORT = process.env.PORT || 3000;
+io.on('connection', (socket) => {
+    console.log('client connected');
+    socket.emit('newMessage', {
+        from: 'Maddy',
+        text: 'data sent',
+        createdAt: 12569874
+    });
+    socket.emit('test', {
+        from: 'test'
+    });
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+    });
+    socket.on('createMessage', function (newEmail) {
+        console.log('create Email', newEmail)
+    });
+})
 
-app.use(express.static(clientPath))
-    .listen(PORT, () => {
-        console.log('app listening on port: ', PORT)
-    })
+app.use(express.static(clientPath));
+
+server.listen(port, () => {
+console.log('port is listening on: ', port)
+});
